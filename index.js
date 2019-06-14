5ca5ec2b47b518e53a0c65229d308b8e4f5ccae9
David Anson <david@dlaa.me>
2019-06-13 22:58:50 -0700

    Add thumbnail capture, photos in a list.

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 "use strict";
 
+const jpegMimeType = "image/jpeg";
+const jpegQuality = 0.8;
+
 class App extends preact.Component {
   constructor() {
     super();
     this.canvasElement = null;
+    this.canvasThumbElement = null;
     this.videoElement = null;
     this.state = {
       pictures: []
     }
   }
 
-  canvasRef(element) {
-    this.canvasElement = element
-  };
-
-  videoRef(element) {
-    this.videoElement = element
-  };
-
   buttonClick() {
     const videoWidth = this.videoElement.videoWidth;
     const videoHeight = this.videoElement.videoHeight;
-    this.canvasElement.width = videoWidth;
-    this.canvasElement.height = videoHeight;
-    this.canvasElement.getContext("2d").drawImage(this.videoElement, 0, 0, videoWidth, videoHeight);
-
+    [
+      [ this.canvasElement, 1 ],
+      [ this.canvasThumbElement, 10 ]
+    ].forEach((canvas) => {
+      const [ element, scale ] = canvas;
+      element.width = videoWidth / scale;
+      element.height = videoHeight / scale;
+      element.getContext("2d").drawImage( this.videoElement, 0, 0, element.width, element.height);
+    });
     const pictures = this.state.pictures;
-    pictures.push(this.canvasElement.toDataURL("image/jpeg", 0.6));
+    pictures.push({
+      image: this.canvasElement.toDataURL(jpegMimeType, jpegQuality),
+      thumb: this.canvasThumbElement.toDataURL(jpegMimeType, jpegQuality),
+    });
     this.setState({ pictures });
   };
 
   render(props, state) {
     const pictureElements = state.pictures.map((picture) => {
-      return preact.h("img", { src: picture, width: 100 });
+      return preact.h(
+        "li",
+        null,
+        preact.h("img", { src: picture.thumb }),
+        preact.h("img", { src: picture.image }));
     });
     return preact.h(
       "div",
       null,
-      preact.h("video", { autoplay: true, playsinline: true, ref: (e) => this.videoRef(e) }),
-      preact.h("canvas", { style: "display:none", ref: (e) => this.canvasRef(e) }),
+      preact.h("canvas", { ref: (e) => this.canvasElement = e }),
+      preact.h("canvas", { ref: (e) => this.canvasThumbElement = e }),
+      preact.h("video", { autoplay: true, playsinline: true, ref: (e) => this.videoElement = e }),
       preact.h("button", { onClick: () => this.buttonClick() }, "Snap"),
-      ...pictureElements);
+      preact.h("ul", null, ...pictureElements));
   };
 
   componentDidMount() {
